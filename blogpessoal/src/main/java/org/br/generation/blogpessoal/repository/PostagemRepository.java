e48d9c1ecba155eb6975b86f144a7bad0535f88e
Isabella Marinho <isabellamarcelomarinho@gmail.com>
2021-10-29 18:34:18 -0300

    Projeto Blog Pessoal

---
+++ blogpessoal/src/main/java/org/br/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,18 @@
+package org.br.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.br.generation.blogpessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+
+}
